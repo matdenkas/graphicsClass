72edabfd3ea4af97252e98d961784160b0d1c554
matdenkas <mateokastl@gmail.com>
2023-10-01 16:26:38 -0500

    spinning cube

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { GL_Wrapper } from "./GLWrapper";
-import { Plane } from "./Primitives"
+import { Plane, Cube } from "./Primitives"
 import { ShaderProgramHelper } from "./ShaderProgramHelper";
 
 const width = 1080;
-const height = 720;
+const height = 1080;
 const canvas = document.getElementById('screen') as HTMLCanvasElement;
 canvas.width = width;
 canvas.height = height;
@@ -15,20 +15,14 @@ prog.attachShaderFromShaderLib(`v_O2W&color`, ShaderProgramHelper.shaderTypes.VE
 prog.attachShaderFromShaderLib(`f_SafeSingleTriWColor`, ShaderProgramHelper.shaderTypes.FRAGMENT);
 glw.attachProgramWrapper(prog);
 
-let p1 = new Plane(glw);
-let p2 = new Plane(glw);
-p2.transform.setTranslation(.5, 0, -.5);
 
+let c = new Cube(glw);
 
 let i = 0;
-let r = 0;
 setInterval(() => {
-    p1.transform.setRotation(i, 0, 0);
-    p2.transform.setRotation(0, -r, 0);
+    c.transform.setRotation(i, 45, i);
     i = (i + 1)%360;
-    r = (r + 2)%360;
-    p1.draw();
-    p2.draw();
+    c.draw();
     glw.reportError();
 
 }, 1000/60);
